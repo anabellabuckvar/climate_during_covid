a665286fcc97f12054a1428f33ee2dcab38f7839
cnaylon <65375124+cnaylon@users.noreply.github.com>
2020-07-27 01:29:20 -0400

    Delete COVID Datasets.R

--- COVID Datasets.R
+++
@@ -1,7 +0,0 @@
-install.packages("tidyverse")
-library(tidyverse)
-
-global_covid <- read.csv(file = 'Global_Mobility_Report.csv')
-
-us_covid <- global_covid %>%
-  filter(country_region_code == "US")
